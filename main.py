d46388c1021b9aca5408ea84c1c47b2e4bf887f1
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-23 11:16:47 +0530

    populated table with data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
+import sqlite3 as sql
 import sys
 
 class MainWindow(QMainWindow):
@@ -19,13 +20,23 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(('ID','Name','Course','Mobile'))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     
-    def load_data():
-        self.table
+    def load_data(self):
+        connection = sql.connect('database.db')
+        result = connection.execute("Select * from students")
+        self.table.setRowCount(0)
+        for row_num , row_data in enumerate(result):
+            self.table.insertRow(row_num)
+            for column_num , data in enumerate(row_data):
+                self.table.setItem(row_num, column_num, QTableWidgetItem(str(data)))
+        connection.close
+        # self.table
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
 main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
