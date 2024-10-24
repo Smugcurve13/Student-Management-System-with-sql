49a11760be7678a525185dd8d8730973a7210c2a
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-24 11:37:41 +0530

    added add student and search in toolbar with icons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QVBoxLayout ,QWidget, QGridLayout, QLineEdit,\
-     QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox
-from PyQt6.QtGui import QAction
+     QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox , QToolBar
+from PyQt6.QtGui import QAction, QIcon
 import sqlite3 as sql
 import sys
 
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow):
         edit_menu_item = self.menuBar().addMenu('&Edit')
 
         # File --> Add Student Action 
-        add_student_action = QAction("Add Student",self)
+        add_student_action = QAction(QIcon("icons/add.png"),"Add Student",self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
@@ -27,7 +27,7 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
 
         # Edit --> Search Action 
-        search_action = QAction('Search',self)
+        search_action = QAction(QIcon("icons/search.png"),'Search',self)
         search_action.triggered.connect(self.search)
         edit_menu_item.addAction(search_action)
 
@@ -36,6 +36,13 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(('ID','Name','Course','Mobile'))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+
+        # Create toolbar and add toolbar elements
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
 
     def load_data(self):
         connection = sql.connect('database.db')
